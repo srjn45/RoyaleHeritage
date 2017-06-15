95f96dffd62a0f1a06430ebe75b92a90474d2567
srjn45 <srajanpathak45@gmail.com>
2017-06-16 00:12:53 +0530

    imported and added layout and gallery module  in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { LayoutModule } from "./layout/layout.module";
+import { GalleryModule } from "./gallery/gallery.module";
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		LayoutModule,
+		GalleryModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
