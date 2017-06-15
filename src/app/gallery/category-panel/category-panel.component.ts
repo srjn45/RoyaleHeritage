dd1ade5d00cdabd8503141c6466ad9de9f8e32a9
srjn45 <srajanpathak45@gmail.com>
2017-06-16 01:15:07 +0530

    created category-panel component to hold category-frame component

---
+++ src/app/gallery/category-panel/category-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-panel',
+  templateUrl: './category-panel.component.html',
+  styleUrls: ['./category-panel.component.css']
+})
+export class CategoryPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
