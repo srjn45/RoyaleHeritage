a6d337917ffb7a194aeb42b568188d7fe6fb6d7b
srjn45 <srajanpathak45@gmail.com>
2017-06-16 01:18:12 +0530

    registered gallery.service and category-panel

--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryFrameComponent } from './category-frame/category-frame.component';
+import { CategoryPanelComponent } from './category-panel/category-panel.component';
+import { GalleryService } from "./gallery.service";
 
 @NgModule({
 	imports: [
 		CommonModule
 	],
-	declarations: [CategoryFrameComponent],
-	exports: [CategoryFrameComponent]
+	declarations: [
+		CategoryFrameComponent,
+		CategoryPanelComponent
+	],
+	exports: [
+		CategoryFrameComponent,
+		CategoryPanelComponent
+	],
+	providers: [
+		GalleryService
+	]
 })
 export class GalleryModule { }
