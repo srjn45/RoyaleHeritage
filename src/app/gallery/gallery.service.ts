e77421f2f714d34f1b33e2a44b5b1f022c7782f3
srjn45 <srajanpathak45@gmail.com>
2017-06-17 23:43:58 +0530

    added getImagesInDir method

--- src/app/gallery/gallery.service.ts
+++ src/app/gallery/gallery.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
 
+import { Http } from "@angular/http";
+
+import 'rxjs/Rx';
+
 @Injectable()
 export class GalleryService {
 
-  constructor() { }
+  constructor(
+    private http: Http
+  ) { }
+
+  public getImagesInDir(dir: String): Promise<String[]> {
+    return this.http.get("assets/api/getImages.php?path=" + dir).map(response => response.json() as String[]).toPromise();
+  }
 
 }
