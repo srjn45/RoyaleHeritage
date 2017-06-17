a2d3e3d19bdf85cc809eaa22d7b4d365937a1ab0
srjn45 <srajanpathak45@gmail.com>
2017-06-17 23:47:03 +0530

    returns the array of names of images available in the directory specified

--- src/assets/api/getImages.php
+++ src/assets/api/getImages.php
@@ -1,5 +1,20 @@
 <?php
 
-$_POST['path'];
+$dir = "../../" . $_GET['path'];
+
+$images = [];
+
+// Open a directory, and read its contents
+if (is_dir($dir)) {
+	if ($dh = opendir($dir)) {
+		while (($file = readdir($dh)) !== false) {
+			if(@is_array(getimagesize($dir . $file))) {
+				array_push($images, $file);
+			}
+		}
+		closedir($dh);
+	}
+}
+echo json_encode($images);
 
 ?>
