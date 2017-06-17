6b4b7cf37f5f774c4a658211862d61e4e5501a3c
srjn45 <srajanpathak45@gmail.com>
2017-06-17 23:40:52 +0530

    registered HttpModule

--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpModule } from "@angular/http";
+
 import { CategoryFrameComponent } from './category-frame/category-frame.component';
 import { CategoryPanelComponent } from './category-panel/category-panel.component';
 import { GalleryService } from "./gallery.service";
